a5fbe8e631f181434f158ebdd5b1d79100b9b4fc
YuutoRaimi <playzyehuda@gmail.com>
2026-03-25 16:17:20 +0700

    Tugas Animasi user list

--- app/userList.tsx
+++ app/userList.tsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
 import { ScrollView, TouchableOpacity, View } from "react-native";
 import { Avatar, Card, Text } from "react-native-paper";
+import Animated, { FadeInDown } from "react-native-reanimated"; // <-- Import Reanimated
 import styles from "./Appstyles";
 import userData from "./data.json";
 
@@ -8,31 +9,36 @@ export default function UserList() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {userData.map((user, index) => (
-        <Card key={index} style={styles.card}>
-          <Link
-            href={{
-              pathname: "/profile",
-              params: {
-                userName: user.name,
-                userEmail: user.email,
-                userPhoto: user.photo_url,
-              },
-            }}
-            asChild
-          >
-            <TouchableOpacity style={styles.cardContent}>
-              <Avatar.Image size={70} source={{ uri: user.photo_url }} />
-              <View style={styles.textContainer}>
-                <Text variant="titleMedium" style={styles.userNameText}>
-                  {user.name}
-                </Text>
-                <Text variant="bodyMedium" style={styles.userEmailText}>
-                  {user.email}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </Link>
-        </Card>
+        <Animated.View
+          key={index}
+          entering={FadeInDown.delay(index * 300).springify()}
+        >
+          <Card style={styles.card}>
+            <Link
+              href={{
+                pathname: "/profile",
+                params: {
+                  userName: user.name,
+                  userEmail: user.email,
+                  userPhoto: user.photo_url,
+                },
+              }}
+              asChild
+            >
+              <TouchableOpacity style={styles.cardContent}>
+                <Avatar.Image size={70} source={{ uri: user.photo_url }} />
+                <View style={styles.textContainer}>
+                  <Text variant="titleMedium" style={styles.userNameText}>
+                    {user.name}
+                  </Text>
+                  <Text variant="bodyMedium" style={styles.userEmailText}>
+                    {user.email}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </Link>
+          </Card>
+        </Animated.View>
       ))}
     </ScrollView>
   );
